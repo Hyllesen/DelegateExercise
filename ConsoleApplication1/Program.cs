d1a6aa3ddebc8786384bdfce5df4bd6e6bf4993e
Stefan Hyltoft <stefanhyltoft@gmail.com>
2016-09-13 10:31:20 +0200

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        delegate int NumberChanger(int n);
+        static int num = 10;
+
+        static void Main(string[] args)
+        {
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            Console.WriteLine(getNum());
+        }
+
+        public static int AddNum(int v)
+        {
+            return num += v;
+        }
+
+        public static int getNum()
+        {
+            return num;
+        }
+    }
+}
